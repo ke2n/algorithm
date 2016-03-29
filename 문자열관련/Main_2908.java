00102ee4cd32570f9b39c4119839d8cdd3febdad
Keens <ke2n@naver.com>
2016-03-29 18:03:45 +0900

    상수

---
+++ 문자열관련/Main_2908.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * 상수
+ * https://www.acmicpc.net/problem/2908
+ *
+ * Created by Keens on 2016-03-29.
+ */
+public class Main_2908 {
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int num_1 = in.nextInt();
+    int num_2 = in.nextInt();
+    int temp_1 = reverse(num_1);
+    int temp_2 = reverse(num_2);
+
+    System.out.println((temp_1 > temp_2)? temp_1 : temp_2);
+  }
+
+  public static int reverse(int num){
+    return (num%10)*100 + (num/10%10)*10 + (num/100);
+  }
+}
